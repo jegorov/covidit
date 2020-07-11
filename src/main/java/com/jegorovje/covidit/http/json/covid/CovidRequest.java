8cf61295aa0392176da6e610d8655c187e7b33a2
Egor Egorov <egorov.dev@yahoo.com>
2020-07-12 02:50:17 +0300

    - [X] Запуск в другой треде рест метода - [X] Добавлена зависимость кафки

--- src/main/java/com/jegorovje/covidit/http/json/covid/CovidRequest.java
+++ src/main/java/com/jegorovje/covidit/http/json/covid/CovidRequest.java
@@ -7,23 +7,28 @@ import com.jegorovje.covidit.engine.data.entity.impl.CovidStatisticEntityImpl;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller("/covid")
 public class CovidRequest {
 
+  private static final Logger log = LoggerFactory.getLogger(CovidRequest.class);
 
   @Get(value = "/country/{name}", produces = MediaType.APPLICATION_JSON)
   public String stocks(String name) {
     try {
-
-      CovidStatisticEntityImpl covidStatisticEntity = CommandContext.getCommandContext()
-          .getEngineConfiguration().getCovidStatisticEntityManager()
-          .findCovidStatisticByCountry(name);
-
-      Gson gson = new GsonBuilder().setPrettyPrinting().create();
-      return gson.toJson(covidStatisticEntity);
+      return CommandContext.getCommandContext().getEngineConfiguration()
+          .getExecutorService().submit(() -> {
+            CovidStatisticEntityImpl covidStatisticEntity = CommandContext.getCommandContext()
+                .getEngineConfiguration().getCovidStatisticEntityManager()
+                .findCovidStatisticByCountry(name);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            return gson.toJson(covidStatisticEntity);
+          }).get();
 
     } catch (Exception e) {
+      log.error("Error", e);
       return "error: " + e.toString();
     }
   }
